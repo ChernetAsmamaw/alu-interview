13ffb26abfb4b9edc719f44df823c838923d16af
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-05-26 18:17:48 +0300

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,27 +2,36 @@
 ''' This program finds the minimum number of
 operations required to get n number of H'''
 
-def minOperations(n):
-    ''' This function finds the minimum number of operations'''
-    # Lets initialize a list to store the number of operations
-    # ls will has a length of n+1 because we will start from 0
-    # n: represents the target number of H we want to get
-    # ls: is a lis of length n+1 that stores the number of ops required
-    # j: is any
-    ls = [0]*(n+1)
-    for i in range(2, n+1):
-        # Since we already have one H in the editor, we will start from 2
-        # Because number of opraions required to get 1 H is 0
-        if n <= 1:
-            return 0
-        # Since number of max-op required is to copy & paste i 1 time
-        # So we will initialize the value of ls[i] to i
-        ls[i] = i
-        # Now we will check if i is divisible by any number (j) from 2 to i-1
-        # If yes, then we will copy & paste the number of times
-        for j in range(2, i):
+
+def min_operations(n):
+    """
+    This function finds the minimum number of operations required to
+    obtain a string of length 'n' consisting of the character 'H'
+    using the following operations:
+    1. Print one 'H'
+    2. Select all 'H's
+    3. Copy the selected 'H's
+    4. Paste the copied 'H's
+    
+    Parameters:
+    n (int): The target number of 'H's in the final string.
+    
+    Returns:
+    int: The minimum number of operations required to obtain a string
+     of length 'n'.
+    """
+    # Initialize a list to store the number of operations
+    # The list will have a length of n+1 because we start from 0
+    ops = [0] * (n + 1)
+    
+    for i in range(2, n + 1):
+        # Find the smallest divisor for i
+        for j in range(2, i + 1):
             if i % j == 0:
-                # This is the min number of opes required to get i number of H
-                ls[i] = ls[j] + (i//j)
-    # Here we return the number of opes required to get n number of H
-    return ls[n]
+                # If i is divisible by j, we can obtain 'i' 'H's by performing
+                # the minimum operations required for 'j' 'H's and then
+                # multiplying by i // j (copy-pasting the 'j' 'H's (i // j) - 1 times).
+                ops[i] = ops[j] + (i // j)
+                break
+    
+    return ops[n]
