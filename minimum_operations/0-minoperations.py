707b73ddaabcddc31a8e41825cf49c4c1b66eb38
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-05-26 18:35:25 +0300

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
-''' This program finds the minimum number of
-operations required to get n number of H'''
+"""
+This function calculates the minimum number of operations required to reach
+a target number 'target_num' by finding all the integer values that 'target_num'
+is divisible by and adding them together.
+
+Parameters:
+target_num (int): The target number to be reached.
+
+Returns:
+int: The minimum number of operations required to reach the target number.
+"""
 
 
-def min_operations(n):
-    """
-    This function finds the minimum number of operations required to
-    obtain a string of length 'n' consisting of the character 'H'
-    using the following operations:
-    1. Print one 'H'
-    2. Select all 'H's
-    3. Copy the selected 'H's
-    4. Paste the copied 'H's
-    
-    Parameters:
-    n (int): The target number of 'H's in the final string.
-    
-    Returns:
-    int: The minimum number of operations required to obtain a string
-     of length 'n'.
-    """
-    # Initialize a list to store the number of operations
-    # The list will have a length of n+1 because we start from 0
-    ops = [0] * (n + 1)
-    
-    for i in range(2, n + 1):
-        # Find the smallest divisor for i
-        for j in range(2, i + 1):
-            if i % j == 0:
-                # If i is divisible by j, we can obtain 'i' 'H's by performing
-                # the minimum operations required for 'j' 'H's and then
-                # multiplying by i // j (copy-pasting the 'j' 'H's (i // j) - 1 times).
-                ops[i] = ops[j] + (i // j)
-                break
-    
-    return ops[n]
+def min_operations(target_num):
+        """This function finds the minimum number of operations
+        required to obtain a length of H """
+
+        if target_num <= 1:
+            return 0
+        divisor = 2
+        sum_of_divisors = 0
+        while divisor <= target_num:
+            if target_num % divisor == 0:
+                sum_of_divisors += divisor
+                target_num = target_num / divisor
+            else:
+                divisor += 1
+        return sum_of_divisors
