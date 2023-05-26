52d4f2c6eabfcd6647ab7e7a9314ace360d7138c
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-05-26 18:59:42 +0300

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,27 +8,27 @@ def minOperations(n):
     """
     In a text file, there is a single character H.
     Your text editor can execute only two operations in this file:
-    Copy All and Paste. 
+    Copy All and Paste.
     Given a number n, write a method that calculates the fewest number
     of operations needed to result in exactly n number of H characters
     in the file
-    
+
     target_num
     - Target number of H we want the find the minimum operations for
-    
+
     divider
     - Iterates over all possible divisors of the target number 'n' and
     check which ones are factors of 'n'. This is used to calculate the
     minimum number of operations required to reach 'n' by finding the
     sum of its factors.
-    
+
     numOfOperations
     - The minimum number of operations required to reach 'n' from 1 H.
     """
 
     if n <= 1:
         return 0
-    
+
     target_num = n
     divider = 2
     numOfOperations = 0
