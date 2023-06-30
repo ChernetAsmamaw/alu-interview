20174f0a31dd4267a6c8a5dbeb3dcff41b8bdfa8
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-30 11:27:15 +0300

    pascal triangle that returns a list of intigers

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,13 @@
     - Write a function that returns a list of lists of integers
     - Returns an empty list if n <= 0
     - You can assume n will be always an integer
-    
     - In the following example, n = 4
         - 1
         - 1 1
         - 1 2 1
         - 1 3 3 1
     - Output: [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]] {4 rows}
-      """
+"""
 
 def pascal_triangle(n):
     """Returns a list of lists of integers representing the Pascal’s
@@ -22,7 +21,7 @@ def pascal_triangle(n):
     for i in range(n - 1):
         temp = [0] + pascal[-1] + [0]
         row = []
-        for j in range(len(pascal[i]) - 1):
+        for j in range(len(pascal[i]) + 1):
             row.append(temp[j] + temp[j + 1])
         pascal.append(row)
     return pascal
